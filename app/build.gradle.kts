6cde96a47aa016cfee69b90db0d0407c758c9a95
aleksandr muzalev <muzalev.as@gmail.com>
2024-03-04 16:29:54 +0300

    Preparation

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+
+plugins {
+	java
+	application
+	id("org.springframework.boot") version "3.3.0-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.4"
+	checkstyle
+	jacoco
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	runtimeOnly("com.h2database:h2")
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("net.datafaker:datafaker:2.0.2")
+	implementation("org.instancio:instancio-junit:3.3.1")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+	compileOnly("org.projectlombok:lombok:1.18.30")
+	annotationProcessor("org.projectlombok:lombok:1.18.30")
+	implementation("com.puppycrawl.tools:checkstyle:10.12.4")
+
+
+	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+checkstyle {
+	toolVersion = "10.3.3"
+}
+
+tasks.withType<Checkstyle>().configureEach {
+	reports {
+		xml.required = false
+		html.required = true
+		html.stylesheet = resources.text.fromFile("config/checkstyle/checkstyle.xml")
+	}
+}
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+application {
+	mainClass = "hexlet.code.AppApplication"
+}
+
+jacoco {
+	toolVersion = "0.8.9"
+	reportsDirectory = layout.buildDirectory.dir("reports/jacoco")
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required = true
+		csv.required = false
+		html.outputLocation = layout.buildDirectory.dir("reports/jacoco")
+	}
+}
+
