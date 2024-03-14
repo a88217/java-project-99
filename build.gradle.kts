4ffb8f0ff4a124a6915db1fd53bffb293efba046
aleksandr muzalev <muzalev.as@gmail.com>
2024-03-14 15:40:43 +0300

    Step 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
 	testCompileOnly("org.projectlombok:lombok:1.18.30")
 	testImplementation("org.springframework.security:spring-security-test")
