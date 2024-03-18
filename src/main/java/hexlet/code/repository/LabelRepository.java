afac534f7b8a888eca988a18a374ade7de604a2e
aleksandr muzalev <muzalev.as@gmail.com>
2024-03-18 18:48:33 +0300

    Step 9 Check 2

--- src/main/java/hexlet/code/repository/LabelRepository.java
+++ src/main/java/hexlet/code/repository/LabelRepository.java
@@ -1,7 +1,6 @@
 package hexlet.code.repository;
 
 import hexlet.code.model.Label;
-import jakarta.persistence.SecondaryTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
