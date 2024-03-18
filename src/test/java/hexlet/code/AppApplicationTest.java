ded2e8197b20e268f44aaa579374191d9723a318
aleksandr muzalev <muzalev.as@gmail.com>
2024-03-18 17:38:28 +0300

    Step 9 Check 1

--- src/test/java/hexlet/code/AppApplicationTest.java
+++ src/test/java/hexlet/code/AppApplicationTest.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+class AppApplicationTest {
+    @Test
+    void contextLoads() {
+    }
 }
